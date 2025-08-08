6deae3017ef956e0b9dbf4b8922063973a21b116
Himanshu <himanshu1401x@gmail.com>
2025-08-08 17:28:33 +0530

    BuilderPattern

---
+++ day24/Task02/BlackConcretePrototype.java
@@ -0,0 +1,2 @@
+package PracticeSet.atlaslearnings.day24.Task02;public class BlackConcretePrototype {
+}
---
+++ day24/Task03/MediaPlayer.java
@@ -0,0 +1,2 @@
+package PracticeSet.atlaslearnings.day24.Task03;public interface MediaPlayer {
+}
