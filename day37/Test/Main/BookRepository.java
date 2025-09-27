33da16e696cfadce9ee7b5071297512900d79515
Himanshu <himanshu1401x@gmail.com>
2025-09-27 13:44:31 +0530

    CRUD Operations in Dynamo

--- day37/Test/Main/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package PracticeSet.atlaslearnings.day37.day37Test.Main;
-
-public class BookRepository {
-    public int getBookCount() {
-        System.out.println("assume calling real getBookCount() from the database.");
-        return 10;
-    }
-}
--- day37/Test/Main/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package PracticeSet.atlaslearnings.day37.day37Test.Main;
-
-public interface UserRepository {
-    User findById(Long id);
-    User save(User user);
-}
