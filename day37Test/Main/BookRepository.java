347efd14eff8f5ac44675ed6b3f1bf321067986e
Himanshu <himanshu1401x@gmail.com>
2025-09-27 12:52:57 +0530

    Adding Test cases for Junit & Mockito

---
+++ day37Test/Main/BookRepository.java
@@ -0,0 +1,8 @@
+package PracticeSet.atlaslearnings.day37Test.Main;
+
+public class BookRepository {
+    public int getBookCount() {
+        System.out.println("assume calling real getBookCount() from the database.");
+        return 10;
+    }
+}
